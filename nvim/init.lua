7032dbb0e50f54a5f979b8df72de0f2291f505f4
Ansel Bobrow <ansel.bobrow@gmail.com>
2025-12-13 14:13:12 -0500

    Use yapf for python editing

--- nvim/init.lua
+++ nvim/init.lua
@@ -612,7 +612,7 @@ require('lazy').setup {
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'yapf' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         html = { 'prettierd', 'prettier', stop_after_first = true },
