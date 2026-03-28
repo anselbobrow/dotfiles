125e5c115d718145293f2e2033d3c3dd7b60669e
Ansel Bobrow <ansel.bobrow@gmail.com>
2026-03-28 15:15:21 -0400

    unbind c-hjkl by updating vim-tmux-nav settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -727,6 +727,10 @@ require('lazy').setup {
   },
   { -- vim tmux navigator
     'christoomey/vim-tmux-navigator',
+    init = function()
+      vim.g.tmux_navigator_no_mappings = 1
+      vim.g.tmux_navigator_disable_when_zoomed = 1
+    end,
     cmd = {
       'TmuxNavigateLeft',
       'TmuxNavigateDown',
