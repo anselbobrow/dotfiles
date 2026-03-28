d0d50258efc7c18fe0039a6834421b15aa2d1e89
Ansel Bobrow <ansel.bobrow@gmail.com>
2026-03-28 15:17:32 -0400

    copy init.lua from main

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,99 +1,29 @@
 -- [[ Options ]]
-
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-
--- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
 vim.o.number = true
 vim.o.relativenumber = true
-
--- Enable mouse mode and set scroll multipliers
 vim.o.mouse = 'a'
 vim.o.mousescroll = 'ver:1,hor:3'
-
--- Sync clipboard between OS and Neovim.
--- Schedule the setting after `UiEnter` because it can increase startup-time.
--- Remove this option if you want your OS clipboard to remain independent.
--- See `:help 'clipboard'`
-vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
-
--- Enable break indent
 vim.o.breakindent = true
-
--- Save undo history
 vim.o.undofile = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.o.ignorecase = true
 vim.o.smartcase = true
-
--- Keep signcolumn on by default
 vim.o.signcolumn = 'yes'
-
--- Decrease update time
 vim.o.updatetime = 250
-
--- Decrease mapped sequence wait time
 vim.o.timeoutlen = 300
-
--- Configure how new splits should be opened
 vim.o.splitright = true
 vim.o.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
--- See `:help 'list'`
--- and `:help 'listchars'`
 vim.o.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Preview substitutions live, as you type!
 vim.o.inccommand = 'split'
-
--- Show which line your cursor is on
 vim.o.cursorline = true
-
--- If performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s)
--- See `:help 'confirm'`
 vim.o.confirm = true
-
--- [[ Keymaps ]]
--- Faster write
-vim.keymap.set('n', '<leader>w', '<cmd>w<CR>')
-
--- Go to end of line (helpful for closing autopairs)
-vim.keymap.set('i', '<c-e>', '<esc>A')
-
--- Clear highlights on search when pressing <Esc> in normal mode
--- See `:help hlsearch`
-vim.keymap.set('n', '<Esc>', function()
-  vim
-    .iter(vim.api.nvim_tabpage_list_wins(0))
-    :filter(function(win_id) return vim.api.nvim_win_get_config(win_id).relative ~= '' end)
-    :each(function(win_id) vim.api.nvim_win_close(win_id, false) end)
-  vim.cmd 'nohlsearch'
-end)
-
--- Paste on a new line
-vim.keymap.set('n', '<leader>p', 'o<esc>p')
-vim.keymap.set('n', '<leader>P', 'O<esc>p')
-
--- HJKL as amplified versions of hjkl + rebind existing actions
-vim.keymap.set({ 'n', 'v' }, 'H', '0^')
-vim.keymap.set({ 'n', 'v' }, 'L', '$')
-vim.keymap.set({ 'n', 'v' }, 'J', '6j')
-vim.keymap.set({ 'n', 'v' }, 'K', '6k')
-vim.keymap.set({ 'n', 'v' }, 'M', 'J') -- mnemonic: [M]erge
-
--- Additional filetypes
-vim.filetype.add {
-  filename = {
-    ['Caddyfile'] = 'caddy',
-  },
-}
+-- Schedule the setting after `UiEnter` because it can increase startup-time.
+vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
 
 -- [[ Folding ]]
 vim.o.foldmethod = 'expr'
@@ -103,8 +33,27 @@ vim.o.foldlevel = 99
 vim.o.foldlevelstart = 1
 vim.o.foldnestmax = 2
 
+-- [[ Keymaps ]]
+vim.keymap.set('n', '<leader>w', '<cmd>w<CR>')
+vim.keymap.set('i', '<c-e>', '<esc>A')
+vim.keymap.set('n', '<Esc>', function()
+  vim
+    .iter(vim.api.nvim_tabpage_list_wins(0))
+    :filter(function(win_id) return vim.api.nvim_win_get_config(win_id).relative ~= '' end)
+    :each(function(win_id) vim.api.nvim_win_close(win_id, false) end)
+  vim.cmd 'nohlsearch'
+end)
+vim.keymap.set('n', '<leader>p', 'o<esc>p')
+vim.keymap.set('n', '<leader>P', 'O<esc>p')
+vim.keymap.set({ 'n', 'v' }, 'H', '0^')
+vim.keymap.set({ 'n', 'v' }, 'L', '$')
+vim.keymap.set({ 'n', 'v' }, 'J', '6j')
+vim.keymap.set({ 'n', 'v' }, 'K', '6k')
+vim.keymap.set({ 'n', 'v' }, 'M', 'J')
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open [D]iagnostic under cursor' })
+
 -- [[ Diagnostics ]]
--- See :help vim.diagnostic.Opts
 vim.diagnostic.config {
   update_in_insert = false,
   severity_sort = true,
@@ -121,33 +70,39 @@ vim.diagnostic.config {
   -- Auto open the float, so you can easily read the errors when jumping with `[d` and `]d`
   jump = { float = true },
 }
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
-vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open [D]iagnostic under cursor' })
+
+-- [[ Filetype mappings ]]
+vim.filetype.add {
+  filename = {
+    ['Caddyfile'] = 'caddy',
+  },
+}
 
 -- [[ Highlight when yanking text ]]
--- Try it with `yap` in normal mode
--- See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function() vim.highlight.on_yank() end,
 })
 
--- [[ Install `lazy.nvim` plugin manager ]]
--- See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+-- [[ Install `lazy.nvim` ]]
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
   local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-  if vim.v.shell_error ~= 0 then error('Error cloning lazy.nvim:\n' .. out) end
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+      { out, 'WarningMsg' },
+      { '\nPress any key to exit...' },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
-
----@type vim.Option
-local rtp = vim.opt.rtp
-rtp:prepend(lazypath)
+vim.opt.rtp:prepend(lazypath)
 
 -- [[ Configure and install plugins ]]
-
 require('lazy').setup {
   { -- nvim-lspconfig
     'neovim/nvim-lspconfig',
@@ -292,7 +247,7 @@ require('lazy').setup {
         },
         eslint = {},
         jsonls = {},
-        yaml_language_server = {},
+        cssls = {},
         emmet_language_server = {
           filetypes = { 'html', 'typescriptreact', 'javascriptreact' },
         },
@@ -347,11 +302,12 @@ require('lazy').setup {
       vim.list_extend(ensure_installed, {
         'prettierd', -- for formatting web stuff
         'jdtls', -- Set up with nvim-jdtls, so not added to servers list
-      }
+        'yapf', -- for python formatting
+      })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
-      for name, server in pairs(servers) do
-        vim.lsp.config(name, server)
+      for name, cfg in pairs(servers) do
+        vim.lsp.config(name, cfg)
         vim.lsp.enable(name)
       end
     end,
@@ -515,50 +471,22 @@ require('lazy').setup {
       'nvim-lua/plenary.nvim',
       { -- If encountering errors, see telescope-fzf-native README for installation instructions
         'nvim-telescope/telescope-fzf-native.nvim',
-
-        -- `build` is used to run some command when the plugin is installed/updated.
-        -- This is only run then, not every time Neovim starts up.
         build = 'make',
-
-        -- `cond` is a condition used to determine whether this plugin should be
-        -- installed and loaded.
         cond = function() return vim.fn.executable 'make' == 1 end,
       },
       { 'nvim-telescope/telescope-ui-select.nvim' },
-
-      -- Useful for getting pretty icons, but requires a Nerd Font.
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
     },
     config = function()
-      -- Telescope is a fuzzy finder that comes with a lot of different things that
-      -- it can fuzzy find! It's more than just a "file finder", it can search
-      -- many different aspects of Neovim, your workspace, LSP, and more!
-      --
-      -- The easiest way to use Telescope, is to start by doing something like:
-      --  :Telescope help_tags
-      --
-      -- After running this command, a window will open up and you're able to
-      -- type in the prompt window. You'll see a list of `help_tags` options and
-      -- a corresponding preview of the help.
-      --
-      -- Two important keymaps to use while in Telescope are:
-      --  - Insert mode: <c-/>
-      --  - Normal mode: ?
-      --
-      -- This opens a window that shows you all of the keymaps for the current
-      -- Telescope picker. This is really useful to discover what Telescope can
-      -- do as well as how to actually do it!
-
-      -- [[ Configure Telescope ]]
-      -- See `:help telescope` and `:help telescope.setup()`
       require('telescope').setup {
-        -- You can put your default mappings / updates / etc. in here
-        --  All the info you're looking for is in `:help telescope.setup()`
-        --
         defaults = {
           path_display = { shorten = 2 },
         },
-        -- pickers = {}
+        pickers = {
+          help_tags = {
+            mappings = { i = { ['<cr>'] = 'select_vertical' } },
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
@@ -570,45 +498,36 @@ require('lazy').setup {
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
 
-      -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sn', function() builtin.find_files { cwd = vim.fn.stdpath 'config' } end, { desc = '[S]earch [N]eovim files' })
       vim.keymap.set('n', '<leader>st', builtin.git_status, { desc = '[S]earch Git s[T]atus' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<leader>s.', builtin.resume, { desc = '[S]earch [R]esume ("." for repeat)' })
+      vim.keymap.set('n', '<leader>s.', builtin.resume, { desc = '[S]earch Resume ("." for repeat)' })
       vim.keymap.set('n', '<leader>sr', builtin.oldfiles, { desc = '[S]earch Recent Files' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-
-      -- Slightly advanced example of overriding default behavior and theme
-      vim.keymap.set('n', '<leader>/', function()
-        -- You can pass additional configuration to Telescope to change the theme, layout, etc.
-        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-          winblend = 10,
-          previewer = false,
-        })
-      end, { desc = '[/] Fuzzily search in current buffer' })
-
-      -- It's also possible to pass additional configuration options.
-      --  See `:help telescope.builtin.live_grep()` for information about particular keys
+      vim.keymap.set(
+        'n',
+        '<leader>/',
+        function()
+          builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+            winblend = 10,
+            previewer = false,
+          })
+        end,
+        { desc = '[/] Fuzzily search in current buffer' }
+      )
       vim.keymap.set(
         'n',
         '<leader>s/',
-        function()
-          builtin.live_grep {
-            grep_open_files = true,
-            prompt_title = 'Live Grep in Open Files',
-          }
-        end,
+        function() builtin.live_grep { grep_open_files = true, prompt_title = 'Live Grep in Open Files' } end,
         { desc = '[S]earch [/] in Open Files' }
       )
-
-      -- Shortcut for searching your Neovim configuration files
-      vim.keymap.set('n', '<leader>sn', function() builtin.find_files { cwd = vim.fn.stdpath 'config' } end, { desc = '[S]earch [N]eovim files' })
     end,
   },
   { -- conform (formatters)
@@ -641,7 +560,6 @@ require('lazy').setup {
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
         python = { 'yapf' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
@@ -809,6 +727,10 @@ require('lazy').setup {
   },
   { -- vim tmux navigator
     'christoomey/vim-tmux-navigator',
+    init = function()
+      vim.g.tmux_navigator_no_mappings = 1
+      vim.g.tmux_navigator_disable_when_zoomed = 1
+    end,
     cmd = {
       'TmuxNavigateLeft',
       'TmuxNavigateDown',
